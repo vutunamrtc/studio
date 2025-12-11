53a6c14ae8bb08b4806832a74c3cba3627b2e9bb
Vũ Nam <vutunam.rtc@gmail.com>
2025-12-11 02:22:08 +0000

    vẫn bị che mất đẩy phần điều khuyển xuống qua phần header các icon cần c

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -404,7 +404,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto pt-2 group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:overflow-hidden",
         className
       )}
       {...props}
