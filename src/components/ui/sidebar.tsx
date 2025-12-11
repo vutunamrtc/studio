9ac5849dad8a0bb6b7392a70861627dbfa8e4d5c
Vũ Nam <vutunam.rtc@gmail.com>
2025-12-11 02:30:30 +0000

    phần này cần dịch sang phải thảng hàng với icon tiêu đề ở trên và cần xu

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -404,7 +404,7 @@ const SidebarContent = React.forwardRef<
       ref={ref}
       data-sidebar="content"
       className={cn(
-        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto pt-2 group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:overflow-hidden",
+        "flex min-h-0 flex-1 flex-col gap-2 overflow-auto pt-2 group-data-[collapsible=icon]:items-center group-data-[collapsible=icon]:overflow-hidden group-data-[collapsible=icon]:pt-4",
         className
       )}
       {...props}
@@ -505,7 +505,7 @@ const SidebarMenuItem = React.forwardRef<
   <li
     ref={ref}
     data-sidebar="menu-item"
-    className={cn("group/menu-item relative", className)}
+    className={cn("group/menu-item relative group-data-[collapsible=icon]:flex group-data-[collapsible=icon]:justify-center", className)}
     {...props}
   />
 ))
