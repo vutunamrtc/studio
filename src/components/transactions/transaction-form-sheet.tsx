9b524635e06b02a82a86dd644810bb02a4655d71
Vũ Nam <vutunam.rtc@gmail.com>
2025-12-11 03:59:09 +0000

    phần ngày này khi chọn đang bị lỗi (_for element <Button>_)

--- src/components/transactions/transaction-form-sheet.tsx
+++ src/components/transactions/transaction-form-sheet.tsx
@@ -245,6 +245,9 @@ export default function TransactionFormSheet({
                         mode="single"
                         selected={field.value}
                         onSelect={field.onChange}
+                        disabled={(date) =>
+                          date > new Date() || date < new Date('1900-01-01')
+                        }
                         initialFocus
                       />
                     </PopoverContent>
